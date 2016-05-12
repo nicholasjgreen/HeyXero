01e9b6679fb7c9e18a9afdad06bbcd5862aa4a40
Scott Potter <scott.potter@xero.com>
2016-05-12 17:31:35 +1200

    Adding HowMuchMoneyDoIHave endpoint

--- scripts/how-much-money-do-i-have.coffee
+++ scripts/how-much-money-do-i-have.coffee
@@ -1,1 +1,34 @@
-module.exports = (operation) ->
+Promise = require("bluebird");
+XeroConnection = require('./xero-connection');
+
+module.exports = {
+  doRequest: () ->
+    promise = new Promise (resolve, reject) ->
+      # https://api.xero.com/api.xro/2.0/reports/BankSummary
+      XeroConnection().call 'GET', 'reports/BankSummary', null, (err, json) ->
+        if(err)
+          reject
+        else
+          resolve(json)
+
+    return promise;
+
+  parseResponse: (jsonResponse) ->
+    # Filter and map to array of array
+    cellRows = jsonResponse.Reports[0].Rows.filter((row) -> row.RowType == "Section" && row.Rows[0].RowType == "Row").map((row) -> row.Rows[0].Cells)
+    if (cellRows.length > 0)
+      cellRows.map( (cellRow) ->
+        {
+          #First cell's Value
+          accountName: cellRow[0].Value
+          # Last cell
+          closingBalance: cellRow.slice(-1)[0].Value
+        }
+      )
+
+  formatAnswer: (answer) ->
+    formattedAnswer = "\n"
+    answer.forEach((row) -> formattedAnswer = formattedAnswer + "#{row.accountName}: #{row.closingBalance}\n")
+    formattedAnswer
+
+}
---
+++ tests/how-much-money-do-i-have-test.js
@@ -0,0 +1,35 @@
+var assert = require('assert');
+var howMuchMoneyDoIHave = require('../js/how-much-money-do-i-have');
+var FS = require('fs');
+
+
+describe('HowMuchMoneyDoIHave operation', function () {
+
+  describe('parsing a json response', function () {
+    it('should return accountName and closingBalance', function () {
+      var testResponse = JSON.parse(FS.readFileSync('./tests/testdata/HowMuchMoneyDoIHave.json'));
+      var cells = howMuchMoneyDoIHave.parseResponse(testResponse);
+      assert.equal(cells.length, 1);
+      assert.equal(cells[0].accountName, 'Checking Account');
+      assert.equal(cells[0].closingBalance, '-4946.33');
+    });
+  });
+
+  describe('formatting an answer', function () {
+    it('should format properly', function () {
+      var answer = [
+        {
+          accountName: 'Bank Account',
+          closingBalance: '200.00'
+        },
+        {
+          accountName: 'Checking Account',
+          closingBalance: '-4946.33'
+        }
+      ];
+      var formattedResponse = howMuchMoneyDoIHave.formatAnswer(answer);
+      assert.equal(formattedResponse, "\nBank Account: 200.00\nChecking Account: -4946.33\n");
+    });
+  });
+
+});
